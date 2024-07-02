766cb779e4882cfc458f2c9b76eff268121ba11b
Desislava Naydenova <deskata.01@gmail.com>
2024-07-02 10:29:13 +0000

    Create the database and populate it to python

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 import os
-from taskmanager import app
+from taskmanager import app, db
+
+
+with app.app_context():
+    db.create_all()
+    # You can also add any other initialization code here
 
 
 if __name__ == "__main__":
