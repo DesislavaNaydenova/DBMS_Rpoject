406141008a124decb3c6d6714c14818f7788ce40
Desislava Naydenova <deskata.01@gmail.com>
2024-07-03 11:40:20 +0000

    Add initialization to be able to vew the tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -12,4 +12,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // select initialization
     let selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
+
+    // colapsible initialization
+    let colapsible = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(colapsible);
   });
