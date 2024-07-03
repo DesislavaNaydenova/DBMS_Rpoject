b5a239b5b22a41e56b02d9cf6904a2c57b7c2261
Desislava Naydenova <deskata.01@gmail.com>
2024-07-03 10:15:37 +0000

    Add a datepicker to the js file

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialisation
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+    // datepicker initalization
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, 
+      format_ "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    );
   });
